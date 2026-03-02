92deab5cd71e042a1d329b169da2c094a5a132d8
Tyler Mayfield <Tyler.Mayfield@evergreenps.org>
2026-03-02 11:29:30 -0800

    finished week3/day1

--- 3_crew/debate/src/debate/crew.py
+++ 3_crew/debate/src/debate/crew.py
@@ -1,5 +1,6 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import FileWriterTool
 
 
 @CrewBase
@@ -14,32 +15,37 @@ class Debate():
     def debater(self) -> Agent:
         return Agent(
             config=self.agents_config['debater'],
-            verbose=True
+            verbose=True,
+            tools=[FileWriterTool()]
         )
 
     @agent
     def judge(self) -> Agent:
         return Agent(
             config=self.agents_config['judge'],
-            verbose=True
+            verbose=True,
+            tools=[FileWriterTool()]
         )
 
     @task
     def propose(self) -> Task:
         return Task(
             config=self.tasks_config['propose'],
+            tools=[FileWriterTool()]
         )
 
     @task
     def oppose(self) -> Task:
         return Task(
             config=self.tasks_config['oppose'],
+            tools=[FileWriterTool()]
         )
 
     @task
     def decide(self) -> Task:
         return Task(
             config=self.tasks_config['decide'],
+            tools=[FileWriterTool()]
         )
 
 
